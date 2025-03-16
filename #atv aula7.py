f121565d03c833b9c49ead401ee1e959149fbd48
juan23-goat <carlojuanjc48@gmail.com>
2025-03-16 13:48:53 -0300

    atv aula 7 juan (16/03/2025)

---
+++ #atv aula7.py
@@ -0,0 +1,27 @@
+
+#atividade dia aula 7
+
+#1
+numero1 = int(input("digite um numero inteiro:"))
+for i in range (numero1 , -1,-1):
+ print(i)
+
+
+#2 
+
+numero = int(input("Digite um número: "))
+soma = 0
+i = 1
+while i <= numero:
+    soma += i  
+    i += 1  
+print(f"A soma dos números de 1 até {numero} é {soma}")
+
+
+
+
+
+#3
+numero3 = int(input("digite um numero:"))
+for i in range(1,11):
+  print(f"{numero3} + {i}={numero3+i}") 
