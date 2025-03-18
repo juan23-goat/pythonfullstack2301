add1753c2213ceb891020101b011bc59662ec604
juan23-goat <carlojuanjc48@gmail.com>
2025-03-17 21:37:01 -0300

    aula 8 .js

---
+++ aula8.js
@@ -0,0 +1,44 @@
+//1 
+{ 
+prompt(nota1,"me diga sua primeira nota:")
+prompt(nota2, "me diga sua segunda nota:")
+   function cauculoMedia(nota1 , nota2) {
+ let media = nota1 / nota2 *2 
+}
+  if (media => 7.0) {
+    return "aprovado"
+}
+  else {
+    return "reprovado"
+
+console.log(media)
+
+}}
+ 
+//2 soma dos numeros pares
+function numerospares(n1 , n2){
+for 
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+prompt(numerospares, "digite dois numeros")
+
+
