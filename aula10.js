4b2438d339c05668b55e6ece35b1dc7d658979df
juan23-goat <carlojuanjc48@gmail.com>
2025-03-21 21:36:44 -0300

    aula10

---
+++ aula10.js
@@ -0,0 +1,17 @@
+//1
+function somadamatriz(matriz1 ,matriz2)
+
+for(let i =0; i < matriz1.lenght; i++);
+   for(let i2 =0;i2 < matriz2.length;i2++);
+matriz3(matriz1+matriz2);
+
+
+
+
+let matriz1 = [[1,2,3],[6,7,8] ,[9,10,11]]
+let matriz2 = [[11,12,13],[14,15,16],[18,19,20]]
+
+
+
+
+//2
